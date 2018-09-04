f31fe31d552ac37431531ef8e791b22aed823b4a
Philip Freude <philipfreude@gmail.com>
2018-09-04 21:43:44 +0200

    update normalization

--- CIFAR.py
+++ CIFAR.py
@@ -196,8 +196,8 @@ def get_7_block_model(activation):
 if __name__ == '__main__':
     # get data
     (x_train, y_train), (x_test, y_test) = cifar100.load_data()
-    x_train /= 255
-    x_test /= 255
+    x_train /= 255.0
+    x_test /= 255.0
     y_train = to_categorical(y_train)
     y_test = to_categorical(y_test)
     # get datagen
